843bafec4fac5e4ca66e8d96ba239e9270c11033
biersoeckli <meow@biersoeckli.ch>
2025-02-16 14:02:42 +0000

    add phone number input and validation to user profile; update start script for Angular

--- frontend-angular/src/app/user/user-profile/user-profile.component.ts
+++ frontend-angular/src/app/user/user-profile/user-profile.component.ts
@@ -6,6 +6,8 @@ import { CommonService } from 'src/app/shift-common/services/common.service';
 import { UserProfileParams } from './user-profile.params';
 import * as Parse from 'parse';
 
+export const SWISS_PHONE_NUMBER_REGEX = /^([0][1-9][0-9](\s|)[0-9][0-9][0-9](\s|)[0-9][0-9](\s|)[0-9][0-9])$|^(([0][0]|\+)[1-9][0-9](\s|)[0-9][0-9](\s|)[0-9][0-9][0-9](\s|)[0-9][0-9](\s|)[0-9][0-9])$/;
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html'
@@ -14,10 +16,12 @@ export class UserProfileComponent extends BaseComponent<UserProfileParams> {
 
   reactiveForm: FormGroup;
   validated = false;
+  phoneError: string = '';
 
   get firstName() { return this.reactiveForm.get('firstName'); }
   get lastName() { return this.reactiveForm.get('lastName'); }
   get mail() { return this.reactiveForm.get('mail'); }
+  get phone() { return this.reactiveForm.get('phone'); }
 
   constructor(common: CommonService) {
     super(common);
@@ -28,6 +32,7 @@ export class UserProfileComponent extends BaseComponent<UserProfileParams> {
         Validators.email,
         Validators.required
       ]),
+      phone: new FormControl(this.currentUser?.get('phone') ?? '', Validators.required)
     });
   }
 
@@ -35,13 +40,19 @@ export class UserProfileComponent extends BaseComponent<UserProfileParams> {
   @fluffyLoading()
   @fluffyCatch()
   async onSubmit() {
+    this.phoneError = '';
     this.validated = true;
     if (!this.reactiveForm.valid || !this.currentUser) {
       return;
     }
+    if (this.reactiveForm.value.phone && !SWISS_PHONE_NUMBER_REGEX.test(this.reactiveForm.value.phone) || this.reactiveForm.value.phone.length !== 10) {
+      this.phoneError = 'Die Telefonnummer ist in einem ungültigen Format. Hinweis: Schweizer Telefonnummer in folgendem Format: 0791234567';
+      return;
+    }
     this.currentUser.set('firstName', this.reactiveForm.value.firstName);
-    this.currentUser.set('lastName',  this.reactiveForm.value.lastName);
-    this.currentUser.set('email',  this.reactiveForm.value.mail);
+    this.currentUser.set('lastName', this.reactiveForm.value.lastName);
+    this.currentUser.set('email', this.reactiveForm.value.mail);
+    this.currentUser.set('phone', this.reactiveForm.value.phone);
     await this.currentUser.save();
 
     await this.router.navigateByUrl(this.params.returnUrl || '/');
